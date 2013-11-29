436b0406d5203a8a82fc3c73f2f939a76d548e0e
Edwin Espinosa <edwtron@Edwins-MacBook-Pro.local>
2013-11-29 18:27:58 -0500

    Initial Commit

---
+++ PrimeraClase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PrimeraClase
+//
+//  Created by Edwin Espinosa on 11/29/13.
+//  Copyright (c) 2013 edwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
